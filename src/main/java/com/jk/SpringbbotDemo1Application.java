db0fde47fbe2470a7821bf6dbf086c3f1068b0ba
caonma <w320811803.@qq.com>
2018-10-11 17:16:51 +0800

    测试

---
+++ src/main/java/com/jk/SpringbbotDemo1Application.java
@@ -0,0 +1,12 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbbotDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbbotDemo1Application.class, args);
+    }
+}

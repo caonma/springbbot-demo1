6b395f9652df4b60a267970fa1e3d900f0e3f49a
caonma <w320811803.@qq.com>
2018-10-11 18:41:50 +0800

    好

--- src/main/java/com/jk/controller/TestController.java
+++ src/main/java/com/jk/controller/TestController.java
@@ -3,8 +3,8 @@ package com.jk.controller;
 public class TestController {
     public static void main(String[] args) {
       System.out.print("啊哈哈");
-        System.out.print("啊哈哈");
-        System.out.print("啊哈哈");
+        System.out.print("啊");
+        System.out.print("哈哈");
     }
 
 
